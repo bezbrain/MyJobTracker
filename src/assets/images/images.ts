3690e9929cbe1292b42f3cf7b536340db1bbe6c8
bezbrain <belloquadriolawale@gmail.com>
2023-09-04 18:53:47 +0100

    Design for error page

--- src/assets/images/images.ts
+++ src/assets/images/images.ts
@@ -2,5 +2,9 @@
 export const staticImage: string =
   "https://img.freepik.com/free-photo/fun-3d-cartoon-illustration-indian-businessman_183364-114475.jpg?size=626&ext=jpg&ga=GA1.2.2051698419.1692458160&semt=country_rows_v2";
 
+// Error image
+export const errorImg: string =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXlPV1nT9N3vrseo-nSxwwV-GjqHkCkSHe8A&usqp=CAU";
+
 // Logo image
 export const logo: string = "https://i.imgur.com/dyl4BHP.png";
--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,5 +1,78 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { errorImg } from "../assets/images/images";
+
 const Error = () => {
-  return <div>Error page</div>;
+  return (
+    <ErrorWrapper>
+      <img className="pulse" src={errorImg} alt="error-page" />
+      <h3>Oops! Page does not exist</h3>
+      <Link to="/dashboard">
+        <button className="explore-btn error-btn">Go Home</button>
+      </Link>
+    </ErrorWrapper>
+  );
 };
 
 export default Error;
+
+const ErrorWrapper = styled.main`
+  text-align: center;
+
+  img {
+    width: 40%;
+    margin-block: 6rem 2vw;
+  }
+  @media screen and (max-width: 1000px) {
+    img {
+      width: 500px;
+    }
+  }
+  @media screen and (max-width: 600px) {
+    img {
+      width: 80vw;
+    }
+  }
+  @keyframes pulse {
+    0% {
+      transform: scale(1);
+      opacity: 1;
+    }
+    50% {
+      transform: scale(1.2);
+      opacity: 0.7;
+    }
+    100% {
+      transform: scale(1);
+      opacity: 1;
+    }
+  }
+
+  .pulse {
+    animation-name: pulse;
+    animation-duration: 1s;
+    animation-timing-function: ease-in-out;
+    animation-iteration-count: infinite;
+  }
+
+  h3 {
+    color: red;
+    font-size: 20px;
+  }
+
+  .error-btn {
+    background-color: var(--dark);
+    border: none;
+    width: 100px;
+    padding-block: 10px;
+    color: #fff;
+    font-size: 16px;
+    margin: 20px 10px;
+    transition: ease 0.5s all;
+    cursor: pointer;
+  }
+
+  .error-btn:hover {
+    opacity: 0.8;
+  }
+`;
